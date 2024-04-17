c6afebf8e579b2f8b019b6332e05600cea447c63
BrofessorTec <125778428+BrofessorTec@users.noreply.github.com>
2024-04-16 22:00:23 -0400

    Adding python database elements

---
+++ Database/dbmodel.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from pydantic import BaseModel
+
+# SQLAlchemy setup
+Base = declarative_base()
+
+# Database model for Pokémon
+class Pokemon(Base):
+    __tablename__ = 'pokemon'
+
+    id = Column(Integer, primary_key=True, autoincrement=False)
+    name = Column(String)
+    imageurl = Column(String)
+    maxHp = Column(Integer)
+    currHp = Column(Integer)
+
+    # This is the same as overriding ToString() in C#
+    def __repr__(self):
+        return f"<Pokemon(pokedex_number={self.id}, name='{self.name}', imageurl='{self.imageurl}', maxHp='{self.maxHp}', currHp='{self.currHp}')>"
+
+# This is similar to Microsoft.Data.Sqlite - we need an object to represent the database record
+class PokemonCreate(BaseModel):
+    id: int
+    name: str
+    imageurl: str
+    maxHp: int
+    currHp: int
+
+# Here, we set the orm flag so that a database record can be converted to a Pydantic model on a read
+class PokemonRead(PokemonCreate):
+    class Config:
+        orm_mode = True
